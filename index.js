ecfde3b94454b0fdc3ffb4ed005ba386dcf2db19
Filip <filipdopa@yahoo.com>
2022-05-03 22:23:16 +0200

    Implemented ask feature

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import chalk from 'chalk';
+import pkg from 'enquirer';
+
+const { Select } = pkg;
 
 const box = (color) => [
   `
@@ -14,8 +17,8 @@ const box = (color) => [
   `,
 ];
 const randomColor = '#' + Math.floor(Math.random() * 16777215);
-const chosenHueColor = process.argv[3];
-const chosenLuminosity = process.argv[2];
+let chosenHueColor = process.argv[3];
+let chosenSaturation = process.argv[2];
 
 function renderRandomColor() {
   console.log(chalk.hex(randomColor)(box(randomColor)));
@@ -25,22 +28,22 @@ function renderRandomColor() {
 
 function renderLuminousColor() {
   let color;
-  if (chosenLuminosity === 'dark' && chosenHueColor === 'blue') {
+  if (chosenSaturation === 'dark' && chosenHueColor === 'blue') {
     color = '#00008B';
     console.log(chalk.hex(color)(box(color)));
-  } else if (chosenLuminosity === 'light' && chosenHueColor === 'blue') {
+  } else if (chosenSaturation === 'light' && chosenHueColor === 'blue') {
     color = '#ADD8E6';
     console.log(chalk.hex(color)(box(color)));
-  } else if (chosenLuminosity === 'dark' && chosenHueColor === 'red') {
+  } else if (chosenSaturation === 'dark' && chosenHueColor === 'red') {
     color = '#990000';
     console.log(chalk.hex(color)(box(color)));
-  } else if (chosenLuminosity === 'light' && chosenHueColor === 'red') {
+  } else if (chosenSaturation === 'light' && chosenHueColor === 'red') {
     color = '#FF7F7F';
     console.log(chalk.hex(color)(box(color)));
-  } else if (chosenLuminosity === 'dark' && chosenHueColor === 'green') {
+  } else if (chosenSaturation === 'dark' && chosenHueColor === 'green') {
     color = '#013220';
     console.log(chalk.hex(color)(box(color)));
-  } else if (chosenLuminosity === 'light' && chosenHueColor === 'green') {
+  } else if (chosenSaturation === 'light' && chosenHueColor === 'green') {
     color = '#90EE90';
     console.log(chalk.hex(color)(box(color)));
   } else {
@@ -54,6 +57,8 @@ function renderHueColor() {
     console.log(chalk.hex('#0000FF')(box('#0000FF')));
   } else if (chosenHueColor === 'green') {
     console.log(chalk.hex('#00FF00')(box('#00FF00')));
+  } else if (process.argv.includes('ask')) {
+    return;
   }
   console.log(`Ooops! We don't have that color`);
 }
@@ -63,3 +68,34 @@ if (process.argv.length <= 3) {
 } else {
   renderLuminousColor();
 }
+
+async function handleAsk() {
+  const promptAnswers = {
+    color: '',
+    saturation: '',
+  };
+  const promptColor = new Select({
+    name: 'color',
+    message: 'Choose a color',
+    choices: ['red', 'green', 'blue'],
+  });
+
+  const promptSaturation = new Select({
+    name: 'saturation',
+    message: 'Would you like your color to be dark or light?',
+    choices: ['light', 'dark'],
+  });
+
+  const promptColorAnswer = await promptColor.run();
+  const saturationAnswer = await promptSaturation.run();
+  promptAnswers.color = promptColorAnswer;
+  promptAnswers.saturation = saturationAnswer;
+  console.log(promptAnswers);
+
+  chosenHueColor = promptAnswers.color;
+  chosenSaturation = promptAnswers.saturation;
+
+  renderLuminousColor();
+}
+
+process.argv[2] === 'ask' && handleAsk();
